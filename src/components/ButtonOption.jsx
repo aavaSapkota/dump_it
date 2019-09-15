ad07013f174fe6f37540ed47ff2ec1121d35c992
qvnguyen <nguyenquocviet170898@gmail.com>
2019-09-14 20:33:45 -0400

    Edit Homepage

--- src/components/ButtonOption.jsx
+++ src/components/ButtonOption.jsx
@@ -1,20 +1,19 @@
 import React, { Component } from "react";
-import { Button, ButtonGroup, Col, Row } from 'reactstrap';
-import { Link, Route, Switch } from "react-router-dom";
+import { Button } from "reactstrap";
+import { Link } from "react-router-dom";
 import "./ButtonOption.module.css";
 
 export default class ButtonOption extends Component {
-    
-    render() {
-        const {title} = this.props;
-        return (
-            
-            <Link class="link" to="/camera">
-                <Button className="main-btn">{title}</Button>
-            </Link>
-                
-        );
-    }
+  render() {
+    const { title } = this.props;
+    return (
+      <div className="buttonOption">
+        <Link to="/camera">
+          <Button className="main-btn mt-3" size="lg" block>
+            {title}
+          </Button>
+        </Link>
+      </div>
+    );
+  }
 }
-
-        
