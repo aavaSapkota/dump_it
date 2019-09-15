cd4f0197d733b55bf34c0105a3e37bea94173216
qvnguyen <nguyenquocviet170898@gmail.com>
2019-09-15 07:16:41 -0400

    Updates

---
+++ precache-manifest.3b74527666c76b279edc25f34f35d0e8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9761921d72f0e0422d418735c3751e63",
+    "url": "/dump_it/index.html"
+  },
+  {
+    "revision": "403896f59c5c058e61d0",
+    "url": "/dump_it/static/css/2.97e48e18.chunk.css"
+  },
+  {
+    "revision": "594eff4b5120b192cdea",
+    "url": "/dump_it/static/css/main.2d76b0b3.chunk.css"
+  },
+  {
+    "revision": "403896f59c5c058e61d0",
+    "url": "/dump_it/static/js/2.e42623c2.chunk.js"
+  },
+  {
+    "revision": "594eff4b5120b192cdea",
+    "url": "/dump_it/static/js/main.4c90815a.chunk.js"
+  },
+  {
+    "revision": "32665d7d9228d98dbd53",
+    "url": "/dump_it/static/js/runtime~main.b2659557.js"
+  },
+  {
+    "revision": "f2db32f6c526efb5a9c98db828cf0510",
+    "url": "/dump_it/static/media/logo1.f2db32f6.png"
+  }
+]);
